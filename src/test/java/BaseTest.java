6b7780a0af87b6b75697f7ea9e3de283c9395659
Yash Ubana <yashubana789@gmail.com>
2026-08-07 20:53:27 +0530

    added option driver"

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -16,7 +16,7 @@ public class BaseTest {
 	    option.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0");
 
 
-		driver = new ChromeDriver();
+		driver = new ChromeDriver(option);
 		driver.get("http://www.naukri.com/");
 //		driver.manage().window().maximize();
 	}
