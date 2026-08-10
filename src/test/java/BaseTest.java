4c7995cb6ed0cfacb485dfe48267009912e48413
Yash Ubana <yashubana789@gmail.com>
2026-08-10 22:43:19 +0530

    added option in driver

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -17,15 +17,15 @@ public class BaseTest {
 	public ChromeOptions option;
 	@BeforeMethod
 	public void openbrowser() {
-//		option = new ChromeOptions() ;
-//		option.addArguments("--headless=new");
-//		option.addArguments("--window-size=1920,1080");
-//	    option.addArguments("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
-//		option.addArguments("--disable-blink-features=AutomationControlled");
-//		option.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
-//		option.setExperimentalOption("useAutomationExtension", false);
+		option = new ChromeOptions() ;
+		option.addArguments("--headless=new");
+		option.addArguments("--window-size=1920,1080");
+	    option.addArguments("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
+		option.addArguments("--disable-blink-features=AutomationControlled");
+		option.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+		option.setExperimentalOption("useAutomationExtension", false);
 
-		driver = new ChromeDriver();
+		driver = new ChromeDriver(option);
 		driver.get("https://naukri.com"); 
 		
             
