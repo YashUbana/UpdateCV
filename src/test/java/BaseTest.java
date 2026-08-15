0cf8023e8008fab6dc9a3a938ccc2528acc81661
Yash Ubana <yashubana789@gmail.com>
2026-08-15 14:23:40 +0530

    updated BaseTest to directly inject cookie to login directly

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,6 +4,8 @@ import java.io.FileWriter;
 import java.util.Collections;
 import java.util.Set;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -21,16 +23,62 @@ public class BaseTest {
 		
 		option = new ChromeOptions() ;
 		
-		option.addArguments("--headless=new");
-		option.addArguments("--window-size=1920,1080");
-		option.addArguments("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
-		option.addArguments("--disable-blink-features=AutomationControlled");
-		option.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
-		option.setExperimentalOption("useAutomationExtension", false);
+		
+		option.addArguments("--headless=new"); 
+        option.addArguments("--no-sandbox");
+        option.addArguments("--disable-dev-shm-usage");
+        option.addArguments("--disable-blink-features=AutomationControlled");
+        option.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36");
+        
+        
 		
 		driver = new ChromeDriver(option);
 		driver.manage().window().maximize();
-		driver.get("https://www.naukri.com/"); 
+		
+		try {
+			driver.get("https://www.naukri.com/");
+			
+			String rawJsonCookies = System.getenv("NAUKRI_JSON_COOKIES");
+			
+			if(rawJsonCookies == null || rawJsonCookies.trim().isEmpty()) {
+				System.out.println("NAUKRI_JSON_COOKIES enviroment missing");
+				return;
+			}
+			
+			JSONArray cookieArray = new JSONArray(rawJsonCookies);
+			
+			for (int i = 0; i<cookieArray.length(); i++) {
+				JSONObject jsonCookie = cookieArray.getJSONObject(i);
+				
+				String name = jsonCookie.getString("name");
+				String value = jsonCookie.getString("value");
+				
+				String domain = jsonCookie.optString("domain", ".naukri.com");
+				String path = jsonCookie.optString("path", "/");
+				
+				Cookie seleniumCookie = new Cookie.Builder(name, value)
+										.domain(domain)
+										.path(path)
+										.isSecure(jsonCookie.optBoolean("secure", true))
+										.build();
+				driver.manage().addCookie(seleniumCookie);
+												
+				
+			}
+			
+			driver.get("https://www.naukri.com/mnjuser/homepage");
+			
+			System.out.println("Bypassed OTP!");
+			
+			
+		} catch (Exception e) {
+			System.err.println("Automation failed: "+ e.getMessage());
+			e.printStackTrace();
+		}finally {
+			driver.quit();
+		}
+		
+		 
 		
             
 	}
@@ -53,5 +101,7 @@ public class BaseTest {
 	
 	
 }
+
+
 
 
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -6,10 +6,10 @@ public class LoginTest extends BaseTest {
 	@Test
 	public void login() throws IOException {
 		LoginPage page1 = new LoginPage(driver);
-		page1.loginclick1();
-		page1.email_txt();
-		page1.pass_txt();
-		page1.loginclick2();
+//		page1.loginclick1();
+//		page1.email_txt();
+//		page1.pass_txt();
+//		page1.loginclick2();
 		page1.profilebtnclick();
 		page1.deletefile();
 		page1.uploadfile();
