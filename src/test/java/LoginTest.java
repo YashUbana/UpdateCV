fb9f00bce840068aa677d148a853420c16fa39a5
Yash Ubana <yashubana789@gmail.com>
2026-08-16 12:17:51 +0530

    added sleep in LoginTest

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -6,18 +6,21 @@ import utils.Screenshoter;
 public class LoginTest extends BaseTest {
 	
 	@Test
-	public void login() throws IOException {
+	public void login() throws IOException, InterruptedException {
 		LoginPage page1 = new LoginPage(driver);
 		
 //		page1.loginclick1();
 //		page1.email_txt();
 //		page1.pass_txt();
 //		page1.loginclick2();
+		Thread.sleep(2000);
 		page1.profilebtnclick();
 		Screenshoter.takeScreenshot(driver, "LoginTest");
+		Thread.sleep(4000);
 		page1.deletefile();
 		Screenshoter.takeScreenshot(driver, "LoginTest");
 		page1.uploadfile();
+		Thread.sleep(3000);
 		Screenshoter.takeScreenshot(driver, "LoginTest");
 
 	}
